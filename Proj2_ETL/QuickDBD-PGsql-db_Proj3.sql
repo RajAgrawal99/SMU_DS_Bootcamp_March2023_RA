ffce73a709fbd4af79ff2a4d613ef7473b92e1ac
RajAgrawal99 <Rajagrawal@hotmail.com>
2023-06-27 21:15:42 -0500

    project2_ETL_27Jun2023

---
+++ Proj2_ETL/QuickDBD-PGsql-db_Proj3.sql
@@ -0,0 +1,54 @@
+CREATE TABLE "category" (
+    "category_id" VARCHAR(10)   NOT NULL,
+    "category_name" VARCHAR(50)   NOT NULL,
+    CONSTRAINT "pk_category" PRIMARY KEY (
+        "category_id"
+     )
+);
+
+CREATE TABLE "subcategory" (
+    "subcategory_id" VARCHAR(10)   NOT NULL,
+    "subcategory_name" VARCHAR(50)   NOT NULL,
+    CONSTRAINT "pk_subcategory" PRIMARY KEY (
+        "subcategory_id"
+     )
+);
+
+CREATE TABLE "contacts" (
+    "contact_id" VARCHAR(10)   NOT NULL,
+    "first_name" VARCHAR(50)   NOT NULL,
+    "last_name" VARCHAR(50)   NOT NULL,
+    "email" VARCHAR(50)   NOT NULL,
+    CONSTRAINT "pk_contacts" PRIMARY KEY (
+        "contact_id"
+     )
+);
+
+CREATE TABLE "campaign" (
+    "cf_id" INT   NOT NULL,
+    "contact_id" VARCHAR(10)    NOT NULL,
+    "company_name" VARCHAR(100)   NOT NULL,
+    "description" VARCHAR(100)   NOT NULL,
+    "goal" numeric(10,2)   NOT NULL,
+    "pledged" numeric(10,2)   NOT NULL,
+    "outcome" VARCHAR(50)   NOT NULL,
+    "backers_count" INT   NOT NULL,
+    "country" VARCHAR(10)   NOT NULL,
+    "currency" VARCHAR(10)   NOT NULL,
+    "launched_at" DATE   NOT NULL,
+    "deadline" DATE   NOT NULL,
+    "category_id" VARCHAR(10)   NOT NULL,
+    "subcategory_id" VARCHAR(10)   NOT NULL,
+    CONSTRAINT "pk_campaign" PRIMARY KEY (
+        "cf_id"
+     )
+);
+
+ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_contact_id" FOREIGN KEY("contact_id")
+REFERENCES "contacts" ("contact_id");
+
+ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_category_id" FOREIGN KEY("category_id")
+REFERENCES "category" ("category_id");
+
+ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_subcategory_id" FOREIGN KEY("subcategory_id")
+REFERENCES "subcategory" ("subcategory_id");
---
+++ Proj2_ETL/SQL&ERD/QuickDBD-Free Diagram (3).sql
@@ -0,0 +1,74 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/o6hVaM
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+SET XACT_ABORT ON
+
+BEGIN TRANSACTION QUICKDBD
+
+CREATE TABLE [Subcategory] (
+    [subcategory_id] varchar(8)  NOT NULL ,
+    [subcategory] varchar(255)  NOT NULL ,
+    CONSTRAINT [PK_Subcategory] PRIMARY KEY CLUSTERED (
+        [subcategory_id] ASC
+    )
+)
+
+CREATE TABLE [Category] (
+    [category_id] varchar(5)  NOT NULL ,
+    [category] varchar(255)  NOT NULL ,
+    CONSTRAINT [PK_Category] PRIMARY KEY CLUSTERED (
+        [category_id] ASC
+    )
+)
+
+CREATE TABLE [Contacts] (
+    [contact_id] int  NOT NULL ,
+    [first_name] varchar(255)  NOT NULL ,
+    [last_name] varchar(255)  NOT NULL ,
+    [email] varchar(255)  NOT NULL ,
+    CONSTRAINT [PK_Contacts] PRIMARY KEY CLUSTERED (
+        [contact_id] ASC
+    )
+)
+
+CREATE TABLE [Campaign] (
+    [cf_id] int  NOT NULL ,
+    [contact_id] int  NOT NULL ,
+    [company_name] varchar(255)  NOT NULL ,
+    [description] varchar(255)  NOT NULL ,
+    [goal] decimal  NOT NULL ,
+    [pledged] decimal  NOT NULL ,
+    [outcome] varchar(255)  NOT NULL ,
+    [backers_count] int  NOT NULL ,
+    [country] varchar(2)  NOT NULL ,
+    [currency] varchar(3)  NOT NULL ,
+    [launched_date] date  NOT NULL ,
+    [end_date] date  NULL ,
+    [category_id] varchar(5)  NOT NULL ,
+    [subcategory_id] varchar(8)  NOT NULL ,
+    CONSTRAINT [PK_Campaign] PRIMARY KEY CLUSTERED (
+        [cf_id] ASC
+    ),
+    CONSTRAINT [UK_Campaign_contact_id] UNIQUE (
+        [contact_id]
+    )
+)
+
+ALTER TABLE [Subcategory] WITH CHECK ADD CONSTRAINT [FK_Subcategory_subcategory_id] FOREIGN KEY([subcategory_id])
+REFERENCES [Campaign] ([subcategory_id])
+
+ALTER TABLE [Subcategory] CHECK CONSTRAINT [FK_Subcategory_subcategory_id]
+
+ALTER TABLE [Category] WITH CHECK ADD CONSTRAINT [FK_Category_category_id] FOREIGN KEY([category_id])
+REFERENCES [Campaign] ([category_id])
+
+ALTER TABLE [Category] CHECK CONSTRAINT [FK_Category_category_id]
+
+ALTER TABLE [Contacts] WITH CHECK ADD CONSTRAINT [FK_Contacts_contact_id] FOREIGN KEY([contact_id])
+REFERENCES [Campaign] ([contact_id])
+
+ALTER TABLE [Contacts] CHECK CONSTRAINT [FK_Contacts_contact_id]
+
+COMMIT TRANSACTION QUICKDBD
